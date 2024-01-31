eafde587715eb437690554f0b8ba76d0f524822d
Nahidul-Islam-Sayel <islamnahidul0825@gmail.com>
2024-01-31 18:51:38 +0600

    google map add

--- post_to_qrcode.php
+++ post_to_qrcode.php
@@ -29,3 +29,59 @@ function philosopy_button($attributes) {
 add_shortcode('button', 'philosopy_button');
 
 
+
+////nested shortcode
+function philosopy_button2($attributes, $content = null) {
+    $default = array(
+        'type'  => 'primary',
+        'url'   => 'www.google.com',
+        'title' => 'primary button',
+    );
+
+    $button_attributes = shortcode_atts($default, $attributes);
+
+    return sprintf(
+        '<a class="btn btn--%s full-width" href="%s">%s</a>',
+        $button_attributes['type'],
+        esc_url($button_attributes['url']),
+        do_shortcode($content)
+    );
+}
+
+add_shortcode('button2', 'philosopy_button2');
+
+
+
+function uppercase($attributes,$content=''){
+	return strtoupper(do_shortcode($content));
+}
+add_shortcode('uc','uppercase');
+
+
+//////Google map
+
+function fmap($attributes) {
+    $default = array(
+        'place' => 'Dhaka Museum',
+        'width' => '800',
+        'height' => '500'
+    );
+
+    $params = shortcode_atts($default, $attributes);
+
+    $map = <<<EOD
+    <div>
+        <div>
+            <iframe width="{$params['width']}" height="{$params['height']}"
+                    src="https://maps.google.com/maps?q={$params['place']}&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+        </div>
+    </div>
+EOD;
+
+    return $map;
+}
+
+add_shortcode('map', 'fmap');
+
